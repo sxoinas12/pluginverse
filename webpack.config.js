21efb3dd41e7620669dd0e8c2e43d67299be7740
sxoinas12 <nandandor@ghotmail.com>
2020-04-26 16:56:13 +0300

    setup of less & css modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -7,6 +8,15 @@ module.exports = {
     path: path.join(__dirname, '/dist'),
     filename: 'index_bundle.js'
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.[hash].css',
+      chunkFilename: '[id].hash.css'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
   module: {
     rules: [
       {
@@ -15,12 +25,38 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.module.less$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+              localIdentName: '[local]--[hash:base64:8]',
+              camelCase: 'only'
+            }
+          },
+          {
+            loader: 'less-loader'
+          }
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          'css-loader',
+          'less-loader'
+        ],
+        exclude: /\.module.less$/
       }
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-  ]
+  }
 };
