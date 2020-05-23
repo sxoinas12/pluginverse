3ac2cae04aefcc749bc144dd64304a82c6e45c6e
Nikatlas <nikatlas@gmail.com>
2020-05-23 15:45:26 +0300

    Megamenu

---
+++ src/components/MegaMenu/index.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Row, Col } from 'react-grid-system';
+import styles from './styles.module.less';
+
+const MegaMenu = (props) => {
+  const {
+    title,
+    structure
+  } = props;
+
+  const keys = Object.keys(structure);
+  const columns = 12/keys.length;
+  return (
+    <Row className={styles.menuContainer}>
+      <Col>
+        <div className={styles.fixContainer}>
+          <Row>
+            <Col>
+              <div className={styles.title}>{title}</div>
+            </Col>
+          </Row>
+          <Row>
+            {keys.map(key => {
+              const titles = Object.keys(structure[key]);
+              return (
+                <Col xs={columns} className={styles.menuBlock}>
+                  <div className={styles.subtitle}>{key}</div>
+                  {titles.map((title, index) => <a key={index} className={styles.category} href={structure[key][title]}>{title}</a>)}
+                </Col>
+              );
+            })}
+          </Row>
+        </div>
+      </Col>
+    </Row>
+  );
+};
+
+MegaMenu.propTypes = {
+  title: PropTypes.string,
+  structure: PropTypes.shape({})
+};
+
+MegaMenu.defaultProps = {
+  title: "ALL PLUGIN CATEGORIES",
+  structure: {
+    'Category 1': {
+      'Test 1': 'link1',
+      'Test 2': 'link2',
+      'Test 3': 'link3',
+      'Test 4': 'link4'
+    },
+    'Category 2': {
+      'Alpha 1': 'link1',
+      'Alpha 2': 'link2',
+      'Alpha 3': 'link3',
+      'Alpha 4': 'link4'
+    },
+    'Category 3': {
+      'Beta 1': 'link1',
+      'Beta 2': 'link2',
+      'Beta 3': 'link3',
+      'Beta 4': 'link4'
+    }
+  }
+};
+
+export default MegaMenu;
--- src/components/NavBar/component.js
+++ src/components/NavBar/component.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-grid-system';
+import PropTypes from 'prop-types';
+import { Row, Col } from 'react-grid-system';
 import { Link } from "react-router-dom";
 
 import menuIcon from '@assets/icons/menu-icon.svg';
 import styles from './styles.module.less';
 import SearchBox from '../SearchBox';
+import MegaMenu from '../MegaMenu';
 
-const NavBar = () => {
+const NavBar = ({ megaStructure }) => {
   let [menu, setMenu] = React.useState(false);
 
-  return (
+  let navReact = (
     <Row className={styles.navContainer}>
       <Col xs={12} sm={4} className={styles.left}>
         <a onClick={() => setMenu(!menu)}>
@@ -30,6 +32,12 @@ const NavBar = () => {
       </Col>
     </Row>
   );
+  if (menu) navReact = [ navReact, <MegaMenu structure={megaStructure} />];
+  return navReact;
+};
+
+NavBar.propTypes = {
+  megaStructure: PropTypes.shape({})
 };
 
 export default NavBar;
