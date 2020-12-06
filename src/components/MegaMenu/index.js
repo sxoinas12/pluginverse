f3da887cca39df6d9bd1be601d28cad98277bdd6
Nikatlas <nikatlas@gmail.com>
2020-12-06 14:51:05 +0200

    Megamenu

--- src/components/MegaMenu/index.js
+++ src/components/MegaMenu/index.js
@@ -1,38 +1,66 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import classnames from 'classnames';
 import { Container, Row, Col } from 'react-grid-system';
 import styles from './styles.module.less';
 
 const MegaMenu = (props) => {
   const {
     title,
-    structure
+    structure,
+    bundles
   } = props;
   const keys = Object.keys(structure);
   const columns = 12 / keys.length;
+  
+  const bundleKeys = Object.keys(bundles);
   return (
     <Row className={styles.menuContainer}>
       <Col>
         <Container>
-        <div className={styles.fixContainer}>
           <Row>
-            <Col>
-              <div className={styles.title}>{title}</div>
+            <Col xs={12} md={2} className={styles.fixContainer}>
+              <Row>
+                <Col>
+                  <div className={styles.title}>
+                    CURATED BUNDLES
+                  </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col className={classnames(styles.menuBlock, styles.fixContainer)}>
+                  <a href={'/bundles'}>
+                    <div className={styles.bundleTitle}>
+                      All Bundles
+                      <img src={require('@assets/icons/arrow-right-white.svg')} />
+                    </div>
+                  </a>
+                  {bundleKeys.map((bundleName, index) => <a key={index} className={styles.category} href={bundles[bundleName]}>
+                    {bundleName}
+                  </a>)}
+                </Col>
+              </Row>
+            </Col>
+            <Col xs={12} md={10} className={styles.fixContainer}>
+              <Row>
+                <Col>
+                  <div className={styles.title}>{title}</div>
+                </Col>
+              </Row>
+              <Row>
+                {keys.map((key, index) => {
+                  const titles = Object.keys(structure[key]);
+                  return (
+                    <Col xs={12} sm={6} md={4} lg={columns} className={styles.menuBlock} key={index}>
+                      <div className={styles.subtitle}>{key}</div>
+                      {titles.map((cat, index) => <a key={index} className={styles.category} href={structure[key][cat]}>{cat}</a>)}
+                    </Col>
+                  );
+                })}
+              </Row>
             </Col>
           </Row>
-          <Row>
-            {keys.map((key, index) => {
-              const titles = Object.keys(structure[key]);
-              return (
-                <Col xs={columns} className={styles.menuBlock} key={index}>
-                  <div className={styles.subtitle}>{key}</div>
-                  {titles.map((cat, index) => <a key={index} className={styles.category} href={structure[key][cat]}>{cat}</a>)}
-                </Col>
-              );
-            })}
-          </Row>
-        </div>
         </Container>
       </Col>
     </Row>
@@ -46,6 +74,12 @@ MegaMenu.propTypes = {
 
 MegaMenu.defaultProps = {
   title: 'ALL PLUGIN CATEGORIES',
+  bundles: {
+    'Spotify': '/bundle/1',
+    'Volkswagen': '/bundle/1',
+    'Apple': '/bundle/1',
+    'Microsoft': '/bundle/1',
+  },
   structure: {
     'Category 1': {
       'Test 1': 'link1',
