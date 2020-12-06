296e71c1b410f258ecbe0b105a6a34d091199ebe
Nikatlas <nikatlas@gmail.com>
2020-12-06 15:53:00 +0200

    Fix Router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Container } from 'react-grid-system';
 import BaseLoader from '@components/BaseLoader';
 import Pages from '../pages';
@@ -74,19 +74,21 @@ const AppRouter = () => {
 
   return (
     <Router>
-      <LayoutRoute path="/" exact component={Pages.Home} header={Layouts.Header} footer={Layouts.Footer} {...rest} fluid />
-      
-      <LayoutRoute path="/test" exact component={Pages.NikTest} header={Layouts.Header} footer={Layouts.Footer} {...rest} />
-      <Route path="/author/:id" exact component={Pages.AuthorDetails} />
-      <Route path="/authors" exact component={Pages.AuthorList} />
-      <LayoutRoute path="/bundle/:id" exact component={Pages.BundleDetails} header={Layouts.Header} fluid footer={Layouts.Footer} {...rest} theme="dark" />
-      <LayoutRoute path="/bundles" exact component={Pages.BundlesList} header={Layouts.Header} fluid footer={Layouts.Footer} {...rest} />
-      <LayoutRoute path="/category/:id" exact component={Pages.CategoryDetails} header={Layouts.Header} footer={Layouts.Footer} {...rest} />
-      <Route path="/categories" exact component={Pages.CategoryList} />
-      <LayoutRoute path="/plugin/:id" exact component={Pages.PluginDetails} header={Layouts.Header} footer={Layouts.Footer} {...rest} />
-      <LayoutRoute path="/search/:query" exact component={Pages.Search} />
-      <LayoutRoute fluid path="/contact" exact component={Pages.Contact} header={Layouts.Header}  {...rest} />
-      <Route component={Pages.Errors.Error404} />
+      <Switch>
+        <LayoutRoute path="/" exact component={Pages.Home} header={Layouts.Header} footer={Layouts.Footer} {...rest} fluid />
+        
+        <LayoutRoute path="/test" exact component={Pages.NikTest} header={Layouts.Header} footer={Layouts.Footer} {...rest} />
+        <Route path="/author/:id" exact component={Pages.AuthorDetails} />
+        <Route path="/authors" exact component={Pages.AuthorList} />
+        <LayoutRoute path="/bundle/:id" exact component={Pages.BundleDetails} header={Layouts.Header} fluid footer={Layouts.Footer} {...rest} theme="dark" />
+        <LayoutRoute path="/bundles" exact component={Pages.BundlesList} header={Layouts.Header} fluid footer={Layouts.Footer} {...rest} />
+        <LayoutRoute path="/category/:id" exact component={Pages.CategoryDetails} header={Layouts.Header} footer={Layouts.Footer} {...rest} />
+        <Route path="/categories" exact component={Pages.CategoryList} />
+        <LayoutRoute path="/plugin/:id" exact component={Pages.PluginDetails} header={Layouts.Header} footer={Layouts.Footer} {...rest} />
+        <LayoutRoute path="/search/:query" exact component={Pages.Search} />
+        <LayoutRoute fluid path="/contact" exact component={Pages.Contact} header={Layouts.Header}  {...rest} />
+        <Route component={Pages.Errors.Error404} />
+      </Switch>
     </Router>
 
   );
